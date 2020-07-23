1fecbefe99bd6f340512b8b410f0c85c026e9d9a
Manuel Miranda <mmirandasoftware@gmail.com>
2020-07-22 20:35:15 -0700

    End of section 17

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BasketService } from '../../basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
 import { Observable } from 'rxjs';
+import { IUser } from 'src/app/shared/models/user';
+import { AccountService } from '../../account/account.service';
 
 @Component({
     selector: 'app-nav-bar',
@@ -10,10 +12,16 @@ import { Observable } from 'rxjs';
 })
 export class NavBarComponent implements OnInit {
     basket$: Observable<IBasket>;
-    
-    constructor(private basketService: BasketService) {}
+    currentUser$: Observable<IUser>;
+
+    constructor(private basketService: BasketService, private accountService: AccountService) {}
 
     ngOnInit(): void {
         this.basket$ = this.basketService.basket$;
+        this.currentUser$ = this.accountService.currentUser$
+    }
+
+    logout() {
+        this.accountService.logout();
     }
 }
